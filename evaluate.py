c1922094ad14d223369061e7705d52dc67dcac32
jiachen0212 <jiachen0212@163.com>
2018-09-14 15:26:13 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,15 +1,15 @@
 #coding=utf-8
 import numpy as np
 from PIL import Image
-
-
+ 
+ 
 def IOU(lab, res):
-	cla_num = 0
+	cla_num = [0] * 21
 	iou = [0] * 21
 	for r in range(21):
 		n = str(lab.tolist()).count(str(r))  # array 2 list, then count numbers of the cur eme.
 		if n != 0:  # means has the r-ed class
-			cla_num += 1
+			cla_num[r] = 1
 			n_res = str(res.tolist()).count(str(r))
 			if n_res == 0:
 				iou[r] = 0
@@ -23,19 +23,16 @@ def IOU(lab, res):
 				iou[r] = J / (n + n_res - J + 0.0000001)
 	print iou, cla_num
 	return iou, cla_num
-
-
-
-
-			
-
-lab_dir = "/path to gt /"
-label_file = "path to val ids /val_id.txt"
+ 
+ 
+lab_dir = "/path to gt /SegmentationClass/"
+label_file = "/path to /val_id.txt"
 # only need change res label dir
 res_dir = "/path to net result labels/"
 ims = open(label_file)
-ious = []  # 1499 iou lists
-all_cla = 0
+ious =  [[0 for col in range(21)] for row in range(1449)]   # [1449, 21]
+clsaaes = [0 for col in range(21) for row in range(1449)]
+idx = 0
 for im in ims:
 	# print res_dir + im[:-1] + "_blob_0.png"
 	lab = Image.open(lab_dir + im[:-1] + ".png")
@@ -46,15 +43,18 @@ for im in ims:
 	lab = np.array(lab)
 	res = np.array(res)
 	iou, cla_num = IOU(lab, res)
-	ious.append(iou)
-	all_cla += cla_num
-
-miou = [0] * 21
-# ious contain 1499 iou
-for iou in ious:
-	for i in range(21):
-		miou[i] += iou[i]
-MIOU = sum(miou) / (1499 * all_cla)
-print MIOU
-
-
+	ious[idx] = iou
+	clsaaes[idx] = cla_num
+	idx += 1
+ 
+iou = map(sum, zip(*ious))  # sum in col
+cla = map(sum, zip(*clsaaes))
+ 
+res = 0
+for i in range(21):
+	res += iou[i] / cla[i]
+print res / 21
+ 
+ 
+ 
+ 
