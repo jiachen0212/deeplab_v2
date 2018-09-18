58ddd84865367228cd8b0c1ef1f49fe29937c125
jiachen0212 <jiachen0212@163.com>
2018-09-18 09:56:28 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -2,50 +2,60 @@
 import numpy as np
 from PIL import Image
 from scipy.io import loadmat as sio
+import pdb  # bebug
 
 
-def IOU(lab, res):
+def IOU(lab, res, im):
 	cla_num = [0] * 21
 	iou = [0] * 21
 	for r in range(21):
-		n = str(lab.tolist()).count(str(r))  # array 2 list, then count numbers of the cur eme.
+		n = np.sum(lab == r)  # array 2 list, then count numbers of the cur eme.
 		if n != 0:  # means has the r-ed class
+			# pdb.set_trace() 
 			cla_num[r] = 1
-			n_res = str(res.tolist()).count(str(r))
+			n_res = np.sum(res == r)
 			if n_res == 0:
 				iou[r] = 0
-				continue
 			else:
 				J = 0
 				for i in range(lab.shape[0]):
 					for j in range(lab.shape[1]):
-						# be careful of lab col_lab, res     col_lab and res is colorful, lab is 0~20 
-						if res[i, j] == lab[i,j] and lab[i,j] == r:
+						if res[i,j] == r and lab[i,j] == r:
 							J += 1
-				iou[r] = J / (n + n_res - J + 0.0000001)
+				# print n, n_res, J, im[:-1], 'class=', r
+				iou[r] = J / (n + n_res - J + 1e-9)
 	print iou, cla_num
 	return iou, cla_num
 
 
-lab_dir = "/path to gt label/"     
-label_file = "/path to val_id.txt/"
+
+
+
+lab_dir = "/path to gt /" 
+# rgb_dir = "/hik/home/chenjia12/work/deeplab_v2-master/data/VOC2012_orig/JPEGImages/"  
+# col_lab_dir = "/hik/home/chenjia12/work/deeplab_v2-master/data/VOC2012_orig/SegmentationClass/"    
+label_file = "/path to /val_id.txt"
 # only need change res label dir
-res_dir = "/path to net res mat file/"  #.mat
+res_dir = "/path net result label/"  #.mat
 ims = open(label_file)
 ious =  [[0 for col in range(21)] for row in range(1449)]   # [1449, 21]
 clsaaes = [[0 for col in range(21)] for row in range(1449)]
 idx = 0
 for im in ims:
-	# print res_dir + im[:-1] + "_blob_0.png"
 	lab = Image.open(lab_dir + im[:-1] + ".png")
 	mat_file=sio(res_dir + im[:-1] + "_blob_0")
 	mat_file=mat_file['data']
 	res=np.argmax(mat_file,axis=2).astype(np.uint8)
-	res = res.reshape(513, 513)  # 0~20 label data
-	# resize
-	lab = lab.resize(res.shape)
 	lab = np.array(lab)
-	iou, cla_num = IOU(lab, res)
+	# 表示这里很坑, 网络在某个地方把gt转置了......  手动转回来.....
+	lab = np.transpose(lab, [1,0])
+	# print lab.shape
+	height = min(res.shape[0], lab.shape[0])
+	width = min(res.shape[1], lab.shape[1])
+	res = res[:height, :width, 0]
+	lab = lab[:height, :width]
+	# print lab.shape, res.shape, im[:-1]
+	iou, cla_num = IOU(lab, res, im)
 	ious[idx] = iou
 	clsaaes[idx] = cla_num
 	idx += 1
@@ -59,11 +69,5 @@ res = 0
 for i in range(21):
 	res += iou[i] / cla[i]
 print res / 21
-
-
-
-
-
-
 
 
